c610e6cc6fe36d7ffa8951f08aa606eee0f6db12
yobwee-zy <bkingeno07@gmail.com>
2022-04-07 12:44:18 -0700

    My task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+	purs("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
+		
