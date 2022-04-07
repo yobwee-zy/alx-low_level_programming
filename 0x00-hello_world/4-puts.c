e4e3891ca2950533cee12f8ea8502037a795b73f
yobwee-zy <bkingeno07@gmail.com>
2022-04-07 12:50:45 -0700

    My task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  *main - entry point
@@ -7,7 +6,7 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+                return (0);
 }
 		
