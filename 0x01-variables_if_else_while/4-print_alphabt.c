04f31a37a584ead33822ed630ffd232058ee6020
yobwee-zy <bkingeno07@gmail.com>
2022-04-09 11:31:45 -0700

    My task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-
 /**
-  * main 
-  *
-  * Return: Always (Success)
-  */
+*main - prints the alphabet in lowercase, followed by a new line 
+*
+*Return: Always (Success)
+*/
 int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		if (c != 'e' && c != 'q')
-		{
-			putchar(c);
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
-
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+}
+putchar('\n');
+return (0);
 }
