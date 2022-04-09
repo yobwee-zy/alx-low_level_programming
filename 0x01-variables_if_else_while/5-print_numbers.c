625bcaaeb126b9da17870ba180db62bd759dfa77
yobwee-zy <bkingeno07@gmail.com>
2022-04-09 11:42:22 -0700

    My task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-
 /**
-  * main 
-  *
-  * Return: Always (Success)
-  */
+*main - prints the alphabet in lowercase, followed by a new line
+*
+* Return: Always (Success)
+*/
 int main(void)
 {
-	char n;
-
-	for (n = '0'; n <= '9'; n++)
-	{
-		putchar(n);
-	}
-
-	putchar('\n');
-
-	return (0);
-
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
