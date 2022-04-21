f79723f14a6941d863c755e3ec1c3c57499f8775
yobwee-zy <bkingeno07@gmail.com>
2022-04-21 09:10:46 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int _ swaping 2 integer using a pointer
+ * @a:interger
+ * Return:void
+ */
+void swap_int(int *a, int *b)
+{
+int x;
+x= *a;
+*a = *b;
+*b = x;
+return;
+}
