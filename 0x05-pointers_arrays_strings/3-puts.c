b1e1274c9090b220d5db51a6786e00bf3007dd4a
yobwee-zy <bkingeno07@gmail.com>
2022-04-21 11:05:09 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - print a string
+ * _puts -print a string
  * @str:pointer char
  * return:void
  */
@@ -9,6 +9,8 @@ void _puts(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
 }
 _putchar('\n');
 return;
