4886c8878de132ab0730697ff70437e7d409f9fd
yobwee-zy <bkingeno07@gmail.com>
2022-04-21 13:59:09 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,19 @@
-#include "main.h"
+ #include"main.h"
 
-/**
- * _puts -print a string
- * @str: A pointer to an int that will be changed
- *
- * Return:void
- */
-void print_rev(char *s)
-{
-int i;
-i = 0;
-while (s[i] != '\0')
-{
-i ++;
-}
-for (i = i - 1 ; i >= 0; i--)
-{
-_putchar (s[i]);
-}
-_putchar ('\n');
-}
+ /** 
+  * _puts - prints a string, followed by a new line, 
+  * @str: pointer to the string to print 
+  * Return: void 
+ */ 
+   void _puts(char *str) 
+ { 
+ int i = 0; 
+  
+while (str[i])
+	        { 
+_putchar(str[i]); 
+i++; 
+} 
+  
+ _putchar('\n'); 
+ } 
