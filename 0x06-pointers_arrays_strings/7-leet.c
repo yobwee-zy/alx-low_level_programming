c77696619d0a4e3a514813188786df7b03b32520
yobwee-zy <bkingeno07@gmail.com>
2022-04-22 15:19:53 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * leet - transform to leet
+ * @s: char array string type
+ * Return: s transformed
+ */
+
+char *leet(char *s)
+{
+	int i, ii;
+	char s1[] = "aeotl";
+	char S1[] = "AEOTL";
+	char s2[] = "43071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (ii = 0; ii < 5; ii++)
+		{
+
+
