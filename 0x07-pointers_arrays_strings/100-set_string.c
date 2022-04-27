c12baaeeabf4df6021ad8e52a22e1c0384a86d44
yobwee-zy <bkingeno07@gmail.com>
2022-04-27 07:12:25 -0700

    Task 9

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,21 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * print_diagsums -  prints sums
- * @a: a pointer
- * @size: size
+ * set_string - sets pointer val to char
+ * @s: pointer
+ * @to: char
  */
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int i, sum1 = 0, sum2 = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		sum1 += *(a + (size * i + i));
-		sum2 += *(a + (size * i + size - 1 - i));
-	}
-	printf("%d, ", sum1);
-	printf("%d\n", sum2);
+	*s = to;
 }
-
