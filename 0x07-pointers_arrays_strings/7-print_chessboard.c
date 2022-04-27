bf0df3f3aed7fa6d15054b9ea83329178f583c07
yobwee-zy <bkingeno07@gmail.com>
2022-04-27 03:30:23 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,10 +2,9 @@
 
 /**
  * print_chessboard - prints chessboard.
- *
- * @a: rows.
- * Return: nothing.
- */
+ * @a: matrix
+*/
+
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
@@ -14,8 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][j]);
+			_putchar(*(*(i + a) + j));
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
