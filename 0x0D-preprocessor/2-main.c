92f5d1f4892ae59f02e40220fc0d02791dfc060e
yobwee-zy <bkingeno07@gmail.com>
2022-05-13 16:08:32 +0000

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
-#include "0-object_like_macro.h"
-#include "1-pi.h"
-#include "3-function_like_macro.h"
-#include "4-sum.h"
+/**
+ *  * main - prints the name of the file program was compiled from
+ *   * Return: Always 0
+ *    */
 
-#endif 
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
