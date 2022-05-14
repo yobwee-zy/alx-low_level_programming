b4cf50e7f78c1f0b8b93136dc35e1d53a2a8c6ef
yobwee-zy <bkingeno07@gmail.com>
2022-05-14 13:42:01 +0000

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,24 @@
-#include "dog.h"
-#include <stdlib.h>
-
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- *init_dog - initializes a variable type
- * @d: pointer to struct
- * @name: parameter of name
- * @age: parameter of age
- * @owner: parameter of owner
- *
- * Return: nothing.
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+* struct dog -Dog data.
+* @name: Name of the dog
+* @age: age of the dog.
+* @owner: owner of the dog.
+* Desctiption: a structure with the name, age and owner of a dog.
+*/
+
+typedef struct dog
 {
-if (d == NULL)
-return;
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+char *name;
+char *owner;
+float age;
+} 
+dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+#endif
