5b36eebe3fd1f6bb484fcf69b43a4ddbe5e1d066
yobwee-zy <bkingeno07@gmail.com>
2022-05-25 19:21:37 +0000

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,31 +1,21 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 /**
- * print_list - a function that prints all the elements of a list_t list.
- * @h: struct
- * Return: number of nodes
+ * list_len - a function that returns the number
+ *  of elements in a linked list_t list
+ * @h: input
+ * Return: 0
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	int count = 0;
+	int ify = 0;
 
 	if (h == NULL)
 		return (0);
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
+		ify++;
 		h = h->next;
-		count++;
 	}
-
-	return (count);
+	return (ify);
 }
